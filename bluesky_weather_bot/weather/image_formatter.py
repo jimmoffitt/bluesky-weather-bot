0934d5850d9ca882b8224ad504c39d465971fdf9
@snowman <jimmoffitt@yahoo.com>
2026-03-15 18:26:45 -0600

    Remove zip code from image headers; zip code is text-only

--- bluesky_weather_bot/weather/image_formatter.py
+++ bluesky_weather_bot/weather/image_formatter.py
@@ -633,8 +633,6 @@ class WeatherImageFormatter:
 
         img, draw = _new_card(W, H)
         loc = report.location.display_name
-        if report.location.zip_code:
-            loc = f"{loc} ({report.location.zip_code})"
 
         # --- Header ---
         ts     = report.current.timestamp
@@ -858,8 +856,6 @@ class WeatherImageFormatter:
         import re
         c   = report.current
         loc = report.location.display_name
-        if report.location.zip_code:
-            loc = f"{loc} ({report.location.zip_code})"
         cardinal = _deg_to_cardinal(c.wind_direction_deg)
         tags = ""
         m = re.search(r',\s*([A-Z]{2})\b', loc)
